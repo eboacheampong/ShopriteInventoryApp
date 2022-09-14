7ead20471336a73593beaddbd2179765c9bade11
emmanuel <90016711+cejaiy@users.noreply.github.com>
2022-09-14 23:40:37 +0000

    update

--- ShopriteInventoryApp/ReportControl.Designer.cs
+++ ShopriteInventoryApp/ReportControl.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.guna2HtmlLabel6 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // guna2HtmlLabel6
@@ -42,14 +44,27 @@
             this.guna2HtmlLabel6.TabIndex = 37;
             this.guna2HtmlLabel6.Text = "REPORTS";
             // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Image = global::ShopriteInventoryApp.Properties.Resources.f9a8736c48cdaf40d3b9c1721c6f5161;
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(73, 74);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(653, 358);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 38;
+            this.guna2PictureBox1.TabStop = false;
+            // 
             // ReportControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Controls.Add(this.guna2PictureBox1);
             this.Controls.Add(this.guna2HtmlLabel6);
             this.Name = "ReportControl";
             this.Size = new System.Drawing.Size(817, 600);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +73,6 @@
         #endregion
 
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel6;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
     }
 }
